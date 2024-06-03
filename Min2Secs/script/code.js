3dec4147a38515cd7ed1aa45880b22c5fec45aec
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-06-03 15:07:11 +0200

    m2s

--- Min2Secs/script/code.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('convert').addEventListener('click',()=>{
-    let mins = document.getElementById('Minutes').value;
-    const secs = 60;
-
-    document.getElementById('totalSeconds').value = mins * secs + " Seconds"
-    // console.log("I miss u more than anything")
-})
